5012af679e4fd075ed3f400e66c9fc125a568050
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-09-28 14:50:19 +0900

    Initial Commit

---
+++ Spirit/SpiritApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiritApp.swift
+//  Spirit
+//
+//  Created by 구현 on 2023/09/28.
+//
+
+import SwiftUI
+
+@main
+struct SpiritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
