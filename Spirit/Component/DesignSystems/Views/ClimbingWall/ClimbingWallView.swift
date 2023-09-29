b0e6bf00b29d46b7e8b29e122cc9ae88ad1b8002
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-09-29 21:04:38 +0900

    Add: 메인 암벽 '클라이밍 기록하기' 버튼 UI 추가

--- Spirit/Component/DesignSystems/Views/ClimbingWall/ClimbingWallView.swift
+++ Spirit/Component/DesignSystems/Views/ClimbingWall/ClimbingWallView.swift
@@ -2,10 +2,28 @@ import SwiftUI
 
 struct ClimbingWallView: View {
     var body: some View {
-        Rectangle()
-            .frame(width: .infinity, height: 547)
-            .foregroundColor(.clear)
-            .background(Color(red: 0.93, green: 0.93, blue: 0.93))
+        ZStack {
+            Rectangle()
+                .frame(width: .infinity, height: 547)
+                .foregroundColor(.clear)
+                .background(Color(red: 0.93, green: 0.93, blue: 0.93))
+            
+            
+            Button(action: {
+            }) {
+                HStack {
+                    Text("클라이밍 기록하기")
+                        .font(.system(size: 18, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 18)
+                .frame(width: 337, alignment: .center)
+                .background(Color(red: 0.2, green: 0.2, blue:0.2))
+                .cornerRadius(8)
+            }
+            .offset(x: 0.0, y: 220.0)
+        }
+ 
     }
 }
 
