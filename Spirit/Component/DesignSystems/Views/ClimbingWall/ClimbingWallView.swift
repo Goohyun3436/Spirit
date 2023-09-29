cbec1f0e7a2768089cb71b1a9a4b2f83fbd7cc55
Goo Hyun <rngus3436@g.kmou.ac.kr>
2023-09-29 22:15:53 +0900

    Add: 메인 암벽 '리포트, 캡쳐, 암벽 설정' 버튼 UI 추가

--- Spirit/Component/DesignSystems/Views/ClimbingWall/ClimbingWallView.swift
+++ Spirit/Component/DesignSystems/Views/ClimbingWall/ClimbingWallView.swift
@@ -8,6 +8,67 @@ struct ClimbingWallView: View {
                 .foregroundColor(.clear)
                 .background(Color(red: 0.93, green: 0.93, blue: 0.93))
             
+            VStack {
+                Button(action: {
+                }) {
+                    VStack {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 50, height: 50)
+                                .background(Color(red: 0.78, green: 0.86, blue: 0.84))
+                                .cornerRadius(6)
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(Color(red: 0.24, green: 0.38, blue: 0.35))
+                                .cornerRadius(10)
+                            Image("report-btn")
+                                
+                        }
+                        Text("리포트")
+                            .font(.system(size: 12, weight: .semibold))
+                            .foregroundColor(Color(red: 0.17, green: 0.17, blue: 0.17))
+
+                    }
+                }
+                Button(action: {
+                }) {
+                    VStack {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 50, height: 50)
+                                .background(Color(red: 0.78, green: 0.86, blue: 0.84))
+                                .cornerRadius(6)
+                            Image("capture-btn")
+                                
+                        }
+                        Text("캡쳐")
+                            .font(.system(size: 12, weight: .semibold))
+                            .foregroundColor(Color(red: 0.17, green: 0.17, blue: 0.17))
+
+                    }
+                }
+                Button(action: {
+                }) {
+                    VStack {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 50, height: 50)
+                                .background(Color(red: 0.78, green: 0.86, blue: 0.84))
+                                .cornerRadius(6)
+                            Image("setting-btn")
+                                
+                        }
+                        Text("암벽 설정")
+                            .font(.system(size: 12, weight: .semibold))
+                            .foregroundColor(Color(red: 0.17, green: 0.17, blue: 0.17))
+
+                    }
+                }
+            }
+            .offset(x: 140.0, y: 50.0)
             
             Button(action: {
             }) {
